57e68bacfc7d292eadcf7fbd1b62126c2f10f465
Johannes Weber <jwebe0pe@gmail.com>
2018-04-25 13:58:51 +0200

    clean up my brew packaes

--- brew.sh
+++ brew.sh
@@ -40,57 +40,23 @@ brew install gnupg
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
+brew install openssl
 brew install screen
 brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-#brew install aircrack-ng
-#brew install bfg
-#brew install binutils
-#brew install binwalk
-#brew install cifer
-#brew install dex2jar
-#brew install dns2tcp
-#brew install fcrackzip
-#brew install foremost
-#brew install hashpump
-#brew install hydra
-#brew install john
-#brew install knock
-#brew install netpbm
-#brew install nmap
-#brew install pngcheck
-#brew install socat
-#brew install sqlmap
-#brew install tcpflow
-#brew install tcpreplay
-#brew install tcptrace
-#brew install ucspi-tcp # `tcpserver` etc.
-#brew install xpdf
-#brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
 brew install lynx
-#brew install p7zip
-#brew install pigz
-#brew install pv
+brew install gettext
 brew install rename
-#brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-#brew install vbindiff
 brew install zopfli
+brew install s3cmd
 
 # Install other personal apps
 brew install gpg
@@ -115,6 +81,17 @@ brew cask install google-backup-and-sync
 brew cask install vlc
 brew cask install spectacle
 brew cask install kindle
+brew cask install appcleaner
+brew cask install filezilla
+brew cask install google-drive
+brew cask install google-cloud-sdk
+brew cask install java
+brew cask install rambox
+brew cask install sourcetree
+brew cask install thunderbird
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
 
 # Remove outdated versions from the cellar.
 brew cleanup
